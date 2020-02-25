2eb9146e8dc624fd07eca6bfa9a2ecb79e7eae19
ARNO Kole <onlyarno@gmail.com>
2020-02-25 11:18:59 -0600

    unnecessary folder in previous version

---
+++ README.md
@@ -0,0 +1,29 @@
+# FriendFinder
+
+Week 13/HW 11
+
+Friend Finder - Node and Express Servers
+
+Creating a dating app using Node and Express Servers. This full-stack site will take in results from your users' surveys, then compare their answers with those from other users. The app will then display the name and picture of the user with the best overall match.
+
+
+<b>NPM Packages:</b>
+- npm install express - https://www.npmjs.com/package/express
+- npm install body-parser - https://www.npmjs.com/package/body-parser
+- npm install path - https://www.npmjs.com/package/path
+
+<b>Technologies Used:</b>
+- nodeJS
+- Javascript
+- Express
+- HTML
+- Jquery
+- CSS
+
+<b>Prerequisites:</b>
+- Node.js - Download the latest version of Node https://nodejs.org/en/
+- Materialize - Add CDN link to HTML http://materializecss.com/
+
+<b>Built With:</b>
+- Sublime Text - Text Editor
+- Materialize - CSS Framework based on Material Design
---
+++ app/data/friends.js
@@ -0,0 +1,80 @@
+
+//array that holds all the 'friends' that have taken the survey
+
+//initialize starting with 'dummy' data
+var friendList = [
+  {
+    name: 'Lou',
+    profilePic: 'https://www.familychoiceawards.com/wp-content/uploads/2014/04/Wiggleless-Dog-Back-Brace-200-x-200.jpg',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  },
+
+
+  {
+    name: 'Lou',
+    profilePic: 'http://adogbreeds.com/wp-content/uploads/2014/08/Small-White-Pomeranian-Dog-Breeds-200x200.jpg',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  },
+
+
+    {
+    name: 'Lou',
+    profilePic: 'https://i.pinimg.com/236x/4c/05/5d/4c055dcd99404c8fd445fdc5194756cc--cutest-dogs-cute-puppies.jpg',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  },
+
+
+    {
+    name: 'Lou',
+    profilePic: 'http://www.edailypost.com/wp-content/uploads/2012/06/Maltese-Cutest-Dog-Breeds.jpg',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+
+
+];
+
+module.exports = friendList;
